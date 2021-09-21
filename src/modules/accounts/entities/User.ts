23517ecaa92a91736fd4442f1c585a569f564af0
João <joaopfc2764@gmail.com>
2021-09-21 00:31:12 -0300

    Mudanças no avatar

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -22,7 +22,7 @@ class User {
     isAdmin: boolean;
 
     @Column()
-    avatar: string | null;
+    avatar: string;
 
     @CreateDateColumn()
     created_at: Date;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,20 @@
+import multer from 'multer';
 import { Router } from 'express';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import uploadConfig from "../config/upload";
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const userRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
 
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 userRoutes.post("/", createUserController.handle);
+
+userRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle)
 
 export { userRoutes };
