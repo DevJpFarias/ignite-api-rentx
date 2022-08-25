a48146bcfd2dbfe45861337f3c3c9dd439254e48
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-25 16:29:47 -0300

    caso de uso de reset de senha

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,4 +1,4 @@
-import { Repository, getRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { User } from '@modules/accounts/infra/typeorm/entities/User';
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
@@ -6,41 +6,42 @@ import { database } from '@shared/infra/typeorm/helpers/db-connection-helper';
 
 
 class UsersRepository implements IUsersRepository {
-    private repository: Repository<User>;
+  private repository: Repository<User>;
 
-    constructor () {
-        this.repository = database.getRepository(User)
-    }
-    async findById(id: string): Promise<User> {
-        const user = await this.repository.findOne({
-            where: {
-                id
-            }
-        })
-        return user!;
-    }
+  constructor () {
+    this.repository = database.getRepository(User)
+  }
 
-    async create({ name, email, driver_license, password, id, avatar }: ICreateUserDTO): Promise<void> {
-        const user = this.repository.create({
-            name,
-            email,
-            driver_license,
-            password,
-            avatar,
-            id
-        });
+  async create({ name, email, driver_license, password, id, avatar }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      driver_license,
+      password,
+      avatar,
+      id
+    });
 
-        await this.repository.save(user);
-    }
-    
-    async findByEmail(email: string): Promise<User> {
-        const user = await this.repository.findOne({
-            where: {
-                email
-            }
-        })
-        return user!;
-    }
+    await this.repository.save(user);
+  }
+  
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({
+      where: {
+        email
+      }
+    })
+    return user!;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne({
+      where: {
+        id
+      }
+    })
+    return user!;
+  }
 }
 
 export { UsersRepository };
--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -37,6 +37,16 @@ class UsersTokensRepository implements IUsersTokensRepository {
   async deleteById(id: string): Promise<void> {
     await this.repository.delete(id)
   }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = await this.repository.findOneOrFail({
+      where: {
+        refresh_token
+      }
+    })
+
+    return userToken
+  }
 }
 
 export { UsersTokensRepository }
