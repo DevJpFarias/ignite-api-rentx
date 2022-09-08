24db67e8091a69b087ecea96758f94502aa01857
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-09-08 11:52:16 -0300

    provider de Upload

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,11 +3,17 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { EtherealMailProvider } from "../providers/MailProvider/implementations/EtherealMailProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 
 container.registerSingleton<IDateProvider>(
-    "DayjsDateProvider", DayjsDateProvider
+  "DayjsDateProvider", DayjsDateProvider
 )
 
 container.registerInstance<IMailProvider>(
-    "EtherealMailProvider", new EtherealMailProvider()
+  "EtherealMailProvider", new EtherealMailProvider()
+)
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider", LocalStorageProvider
 )
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -3,10 +3,8 @@ import fs from 'fs';
 
 export const deleteFile = async(filename: string) => {
     try {
-        await fs.promises.stat(filename);
-    } catch (error) {
-        return;
+
+    } catch {
+      
     }
-    
-    await fs.promises.unlink(filename);
 }
