5b39d3100f74e135e0dea9f5a3cfa3c30e83f634
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-09-12 16:40:10 -0300

    Criando URL de acesso ao avatar

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import { Router } from 'express';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUser/ProfileUserController';
 
 const userRoutes = Router();
 
@@ -13,8 +14,12 @@ const createUserController = new CreateUserController();
 
 const updateUserAvatarController = new UpdateUserAvatarController();
 
+const profileUserController = new ProfileUserController();
+
 userRoutes.post("/", createUserController.handle);
 
 userRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle)
+
+userRoutes.get("/profile", ensureAuthenticated, profileUserController.handle)
 
 export { userRoutes };
