84a42725ea03dae97d7ff2235936692b8e84765b
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-09-20 14:25:25 -0300

    Configurando o Brabel na aplicação

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -1,13 +1,19 @@
 import { container } from "tsyringe";
 import { EtherealMailProvider } from "../MailProvider/implementations/EtherealMailProvider";
 import { IMailProvider } from "./IMailProvider";
-import { SESMailProvider } from "./implementations/SesMailProvider";
+import { SESMailProvider } from "./implementations/SESMailProvider";
 
-const mailProvider = {
-  ethereal: container.resolve(EtherealMailProvider),
-  ses: container.resolve(SESMailProvider)
+interface IMailProviderIndex {
+  [key: string]: any
 }
 
+const mailProvider: IMailProviderIndex = {
+  'ethereal': container.resolve(EtherealMailProvider),
+  'ses': container.resolve(SESMailProvider)
+}
+
+const MAIL_PROVIDER = (process.env.MAIL_PROVIDER ? process.env.MAIL_PROVIDER : 'ses') as string
+
 container.registerInstance<IMailProvider>(
-  "MailProvider", mailProvider[process.env.MAIL_PROVIDER]
+  "MailProvider", mailProvider[MAIL_PROVIDER]
 )
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,11 +3,17 @@ import { IStorageProvider } from "./IStorageProvider";
 import { LocalStorageProvider } from "./implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./implementations/S3StorageProvider";
 
-const diskStorage = {
+interface diskStorageIndex {
+  [key: string]: any
+}
+
+const diskStorage: diskStorageIndex = {
   local: LocalStorageProvider,
   s3: S3StorageProvider
 }
 
+const disk = (process.env.disk ? process.env.disk : 's3') as string
+
 container.registerSingleton<IStorageProvider>(
-  "StorageProvider", diskStorage[process.env.disk]
+  "StorageProvider", diskStorage[disk]
 )
