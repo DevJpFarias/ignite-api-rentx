22470568f8ed9a76abc63a2529d37547f72c0edd
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-09-26 13:28:03 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 import { connection } from '../typeorm/helpers/connection-helper';
+import cors from 'cors'
 
 import "@shared/container";
 
@@ -22,6 +23,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+
+app.use(cors())
 
 app.use(router);
 
